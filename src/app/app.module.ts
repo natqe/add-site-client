542b2e2d7f59056aafa287eb1809123f33df0fc8
NachmanFarkash <f0527151898@gmail.com>
2018-06-14 01:35:23 +0300

    create the angular project with scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AddSiteHaederComponent } from './add-site-haeder/add-site-haeder.component';
-import { AddSiteFooterComponent } from './add-site-footer/add-site-footer.component';
-import { AddSiteMainComponent } from './add-site-main/add-site-main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AddSiteHaederComponent,
-    AddSiteFooterComponent,
-    AddSiteMainComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
